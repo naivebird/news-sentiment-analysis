4246758a4b603bd2c0040241c9bc830f145f1059
Duc Ho <hoduc1202@gmail.com>
2025-03-10 13:08:59 -0700

    Add text summary

---
+++ part2/transformers_text_summary.py
@@ -0,0 +1,16 @@
+import json
+from transformers import pipeline
+
+
+def transformers_summarize_text(input_path):
+    summarizer_pipeline = pipeline('summarization')
+
+    with open(input_path, "r") as file:
+        articles = json.load(file)
+
+    results = summarizer_pipeline([article["content"] for article in articles], truncation=True)
+    return results
+
+if __name__ == '__main__':
+    results = transformers_summarize_text("data/articles.json")
+    print(results)
